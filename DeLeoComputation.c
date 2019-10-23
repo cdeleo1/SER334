d25129871b9f07bfacb89ae85049515480002a75
Cristi DeLeo <cristi.deleo@asu.edu>
2019-10-23 17:04:11 -0400

    [10/23/2019 Cristi DeLeo] Computation.c task complete

---
+++ DeLeoComputation.c
@@ -0,0 +1,35 @@
+/* 
+ * Computes the total volum for a number of cylinders.
+ * 
+ * Completion time:
+ * 
+ * @author Cristi DeLeo
+ * @version 20191023
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+//INCLUDES
+#include <stdio.h>
+int main(void)
+{
+    int i = 0, numCyl = 0;
+    float height = 0, radius = 0, volume = 0;
+    
+    printf("Enter the number of cylinders (as an integer): ");
+    scanf("%d", &numCyl);
+    
+    for (i = 0; i < numCyl; i++) {
+        printf("\nCylinder %i\n", i + 1);
+        printf("    Enter the height (decimals allowed): ");
+        scanf("%f", &height);
+        printf("    Enter the radius (decimals allowed): ");
+        scanf("%f", &radius);
+        
+        volume = 0.5 * (height * radius);
+        
+        printf("    Volume of Cylinder %i = %f\n", i + 1, volume);       
+    }
+
+    return 0;
+}
+
---
+++ build/Debug/GNU-Linux/DeLeoComputation.o.d
@@ -0,0 +1,1 @@
+build/Debug/GNU-Linux/DeLeoComputation.o: DeLeoComputation.c
